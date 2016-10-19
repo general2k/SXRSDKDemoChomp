d2492f14eb016daef4e29cee7d5a686c67ffa969
qf <qf@qftekimbp.lan>
2016-10-19 15:27:11 +0800

    base

---
+++ SXRSDKDemoChomp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SXRSDKDemoChomp
+//
+//  Created by qf on 16/10/19.
+//  Copyright © 2016年 Keeprapid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
